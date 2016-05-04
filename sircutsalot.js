040da71f4e8891845e473031522c0aed8015f4ab
Paul Borawski <paulborawski14@gmail.com>
2016-05-03 19:38:48 -0700

    server

---
+++ sircutsalot.js
@@ -0,0 +1,16 @@
+var port = 34568;
+var app = require('express')();
+
+app.use(require('compression')());
+app.use(require('body-parser').json());
+app.use(require('body-parser').urlencoded({ extended:true }));
+app.use(require('stylus').middleware(require('path').join(__dirname, 'public')));
+app.use(require('express').static('public'));
+
+require('http').createServer(app).listen(port,function(){
+  console.log('server is running, on port', port)
+});
+
+
+//app.set is for configurations
+//app.use is for middleware
